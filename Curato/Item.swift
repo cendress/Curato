ded7ba3504f541a07c371b27d3cd62343bd50291
cendress <95492688+cendress@users.noreply.github.com>
2026-04-17 14:13:13 -0400

    Initial Commit

---
+++ Curato/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Curato
+//
+//  Created by Christopher Endress on 4/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
